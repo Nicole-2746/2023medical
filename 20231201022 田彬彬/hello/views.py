7a9686c66db72bbfe5b94ab9a319c8d3f223320a
Nicole-2746 <2974332645@qq.com>
2025-10-20 23:05:27 +0800

    修复20231201022 田彬彬目录：移除损坏的子模块配置，添加为普通目录

---
+++ 20231201022 田彬彬/hello/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+
+
+def hello_world(request):
+    """简单的Hello World视图"""
+    return HttpResponse("20231201022，田彬彬!")
+
+
+def hello_name(request, name):
+    """带参数的Hello视图"""
+    return HttpResponse(f"Hello, {name}!")
