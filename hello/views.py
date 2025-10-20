fea920a3e1ec5a91c8fdbeb17b21d54c2149f7cb
Nicole-2746 <2974332645@qq.com>
2025-10-20 13:59:06 +0800

    添加备份项目my_backup_project到仓库

--- hello/views.py
+++ hello/views.py
@@ -7,7 +7,7 @@ def single_page_view(request):
 
 def hello_world(request):
     """Hello World 基础视图"""
-    return HttpResponse("20231201022，田彬彬")
+    return HttpResponse("20231201022，田彬彬!")
 
 def hello_name(request, name):
     """带参数的问候视图"""
